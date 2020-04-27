d415f60004f57bc5e894af2c1a75bd75accae44c
haswee <hasweebanoth@gmail.com>
2020-04-27 17:27:10 +0530

    Update bm25-bash.sh

--- google/bm25-bash.sh
+++ google/bm25-bash.sh
@@ -2,50 +2,24 @@
 rm out.txt
 rm temp.txt
 
+#python change_json.py <Number of files to be returned> <query as required by galago>
 python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(references))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(installation))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(memory) #bm25(management))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(syntax) #bm25(using) #bm25(objects))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(syntax) #bm25(functions))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(syntax) #bm25(variables) #bm25(string) #bm25(declaration))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(syntax) #bm25(comments))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(syntax) #bm25(overview))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(introduction) #bm25(installation))"
-#python change_json.py 10 "#combine(#bm25(f-script) #bm25(programming) #bm25(language) #bm25(history))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(complexity))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(implementation) #bm25(pseudo) #bm25(code))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(operations) #bm25(traversal))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(operations) #bm25(delete))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(operations) #bm25(insert))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(operations) #bm25(range) #bm25(query))"
-#python change_json.py 10 "#combine:0=0.125:1=0.125:2=0.125:3=0.125:4=0.5(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(operations) #bm25(lookup))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(properties))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(properties))"
-#python change_json.py 10 "#combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(definition))"
-#python change_json.py 10 #combine(#bm25(prefix) #bm25(hash) #bm25(tree) #bm25(definition))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(implementation) #bm25(issues))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(implementation) #bm25(pseudo) #bm25(code))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(comparison))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(performance) #bm25(derivation) #bm25(average) #bm25(case))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(performance) #bm25(running) #bm25(time) #bm25(cache) #bm25(use))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(performance) #bm25(space) #bm25(complexity))"
-#python change_json.py 10 "#combine(#bm25(lexicographic) #bm25(search) #bm25(tree) #bm25(features))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(example) #bm25(others))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(example) #bm25(novell’s) #bm25(netware))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(types) #bm25(client/server) #bm25(client) #bm25(server) #bm25(nos))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(types) #bm25(peer-to-peer) #bm25(peer) #bm25(nos))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(common) #bm25(feature))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(history))"
-#python change_json.py 10 "#combine(#bm25(network) #bm25(operating) #bm25(system) #bm25(definition))"
 
-#python change_json.py 10 "#combine(#bm25(semantic) #bm25(interpretation) #bm25(for) #bm25(speech) #bm25(recognition) #bm25(example))"
-#python change_json.py 10 "#combine(#bm25(semantic) #bm25(interpretation) #bm25(for) #bm25(speech) #bm25(recognition) #bm25(generate) #bm25(xml) #bm25(result) #bm25(namespaces))"
-#python change_json.py 10 "#combine(#bm25(semantic) #bm25(interpretation) #bm25(for) #bm25(speech) #bm25(recognition) #bm25(generate) #bm25(xml) #bm25(result) #bm25(attributes) #bm25(value))"
-#python change_json.py 10 "#combine(#bm25(semantic) #bm25(interpretation) #bm25(speech) #bm25(recognition) #bm25(documentation) #bm25(grammar) #bm25(global) #bm25(variable) #bm25(declaration) #bm25(initialization))"
-#python change_json.py 10 "#combine(#bm25(semantic) #bm25(interpretation) #bm25(for) #bm25(speech) #bm25(recognition) #bm25(documentation) #bm25(grammar) #bm25(syntax))"
-#python change_json.py 10 "#combine(#bm25(semantic) #bm25(interpretation) #bm25(speech) #bm25(recognition) #bm25(introduction) #bm25(basic) #bm25(principle))"
+#<location of the galago exe> batch-search <json file created> >> <output file to be written to>
 ../galago batch-search bm25-search.json >> temp.txt
+
+# Since batch-search of bm23 returns lots of unrequired information, the required files locations are mentioned in the last few lines which will be taken by last_n.py file from temp.txt
+
+#python last_n.py <Number of files to be returned> >> <And writes to new file>
 python last_n.py 10 >> out.txt
+
+#out.txt contains the top n closest files to the query.
+#paragraph.py reads the files and converts them into paragraphs. Every paragraph will be in new line. The output is written to numbered_out.txt
+
+#python paragraph.py <name of the google query file without extension>
 python paragraph.py "F-script programming language"
 
-#python corpus_cpy.py "semantic interpretation for speech recognition documentation grammar syntax" ""
+#corpus_cpy.py searches the numbered_out.txt for the most relevant paragraph, using bm25 module for the query mentioned in args
+
+#python corpus_cpy.py <query> <any words you wish to be compulsorily present in the required passage>
+python corpus_cpy.py "F-script programming language" ""
